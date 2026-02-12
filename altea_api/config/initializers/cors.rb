ca8f82050deea150e7fc28aa6a937cd1e2c145cc
Hassyunity <hassy.tsihoarana@gmail.com>
2026-02-13 02:32:21 +0300

    add access for react

--- altea_api/config/initializers/cors.rb
+++ altea_api/config/initializers/cors.rb
@@ -1,9 +1,11 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:5173" # L'adresse de ton projet Vite/React
+    # On ajoute l'URL de Netlify à la liste des origines autorisées
+    origins "http://localhost:5173", "https://altea-flow.netlify.app"
 
     resource "*",
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true # Optionnel : utile si tu gères des sessions/cookies plus tard
   end
 end
